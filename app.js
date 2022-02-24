fd8ebd627eaf2447ccd7c59199cdae50142dedc9
LovelyKein <2312197273@qq.com>
2022-02-24 10:35:02 +0800

    Addjust package.json

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-app.listen(5000, function (error) {
+app.listen(8000, function (error) {
   console.log("Success");
 });
 
